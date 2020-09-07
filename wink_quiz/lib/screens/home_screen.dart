a87a0d78a01ced58a4269a07d24689b9d334fc00
kim-tang2 <rlaxogns4504@gmail.com>
2020-09-08 00:45:56 +0900

    Feature home_screen UI

--- wink_quiz/lib/screens/home_screen.dart
+++ wink_quiz/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:winkquiz/main.dart';
 import 'package:winkquiz/shared/constants.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,16 +18,87 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Wink Quiz App"),
-          backgroundColor: Colors.deepOrangeAccent,
+          backgroundColor: mainColor,
           leading: Container(),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Center(child: Image.asset(winkLogo, width: width * 0.8,)),
+            Center(
+                child: Image.asset(
+              winkLogo,
+              width: width * 0.8,
+            )),
+            Text(
+              "윙크 퀴즈 앱",
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontFamily: 'NotoSans-Bold',
+              ),
+            ),
+            Text(
+              "국민대 최고의 동아리 윙크에 오신 것을 환영합니다.\n 윙크에 대해 더 알아보기 위해 퀴즈를 한 번 풀어볼까요?",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'NotoSans-Regular',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '당신이 윙크의 일원이라는 자격을 증명해보세요.'),
+            _buildStep(width, '0점은 벌로 스터디 3개 더 들어야합니다.'),
+            _buildStep(width, '100점은 기획차장과 데이트권이 있습니다^^'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    color: mainColor,
+                    onPressed: (){},
+                    child: Text(
+                      '문제 풀기',
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: 'NotoSans-Regular'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+          width * 0.048, width * 0.024, width * 0.048, width * 0.024),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(
+            title,
+            style: TextStyle(fontFamily: 'NotoSans-Regular'),
+          ),
+        ],
       ),
     );
   }
--- wink_quiz/lib/shared/constants.dart
+++ wink_quiz/lib/shared/constants.dart
@@ -1,1 +1,4 @@
+import 'dart:ui';
+
 var winkLogo = 'assets/images/wink.png';
+var mainColor = Color.fromRGBO(38, 126, 249, 1);

cee502c9d2b354c0e33d973ab9c5b1a729eb8173
kim-tang2 <rlaxogns4504@gmail.com>
2020-09-08 02:12:24 +0900

    Feature result_screen UI

--- wink_quiz/lib/screens/home_screen.dart
+++ wink_quiz/lib/screens/home_screen.dart
@@ -34,74 +34,77 @@ class _HomeScreenState extends State<HomeScreen> {
     double width = size.width;
     double height = size.height;
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Wink Quiz App"),
-          backgroundColor: mainColor,
-          leading: Container(),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-                child: Image.asset(
-              winkLogo,
-              width: width * 0.8,
-            )),
-            Text(
-              "윙크 퀴즈 앱",
-              style: TextStyle(
-                fontSize: width * 0.065,
-                fontFamily: 'NotoSans-Bold',
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("윙크 퀴즈 앱", style: TextStyle(fontFamily: 'NotoSans-Regular'),),
+            backgroundColor: mainColor,
+            leading: Container(),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                  child: Image.asset(
+                winkLogo,
+                width: width * 0.8,
+              )),
+              Text(
+                "윙크 퀴즈 앱",
+                style: TextStyle(
+                  fontSize: width * 0.065,
+                  fontFamily: 'NotoSans-Bold',
+                ),
               ),
-            ),
-            Text(
-              "국민대 최고의 동아리 윙크에 오신 것을 환영합니다.\n 윙크에 대해 더 알아보기 위해 퀴즈를 한 번 풀어볼까요?",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'NotoSans-Regular',
+              Text(
+                "국민대 최고의 동아리 윙크에 오신 것을 환영합니다.\n 윙크에 대해 더 알아보기 위해 퀴즈를 한 번 풀어볼까요?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'NotoSans-Regular',
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            _buildStep(width, '당신이 윙크의 일원이라는 자격을 증명해보세요.'),
-            _buildStep(width, '0점은 벌로 스터디 3개 더 들어야합니다.'),
-            _buildStep(width, '100점은 기획차장과 데이트권이 있습니다^^'),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: width * 0.036),
-              child: Center(
-                child: ButtonTheme(
-                  minWidth: width * 0.8,
-                  height: height * 0.05,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: RaisedButton(
-                    color: mainColor,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => QuizScreen(
-                                    quizs: quizs,
-                                  )));
-                    },
-                    child: Text(
-                      '문제 풀기',
-                      style: TextStyle(
-                          color: Colors.white, fontFamily: 'NotoSans-Regular'),
+              Padding(
+                padding: EdgeInsets.all(width * 0.048),
+              ),
+              _buildStep(width, '당신이 윙크의 일원이라는 자격을 증명해보세요.'),
+              _buildStep(width, '0점은 벌로 스터디 3개 더 들어야합니다.'),
+              _buildStep(width, '100점은 기획차장과 데이트권이 있습니다^^'),
+              Padding(
+                padding: EdgeInsets.all(width * 0.048),
+              ),
+              Container(
+                padding: EdgeInsets.only(bottom: width * 0.036),
+                child: Center(
+                  child: ButtonTheme(
+                    minWidth: width * 0.8,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: RaisedButton(
+                      color: mainColor,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => QuizScreen(
+                                      quizs: quizs,
+                                    )));
+                      },
+                      child: Text(
+                        '문제 풀기',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'NotoSans-Regular'),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- wink_quiz/lib/screens/quiz_screen.dart
+++ wink_quiz/lib/screens/quiz_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:winkquiz/models/quiz.dart';
+import 'package:winkquiz/screens/result_screen.dart';
 import 'package:winkquiz/shared/constants.dart';
 import 'package:winkquiz/widgets/problem_widget.dart';
 
@@ -111,7 +112,13 @@ class _QuizScreenState extends State<QuizScreen> {
                       ? null
                       : () {
                           if (_currentIndex == widget.quizs.length - 1) {
-                            return null;
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ResultScreen(
+                                          answers: _answers,
+                                          quizs: widget.quizs,
+                                        )));
                           } else {
                             _answerState = [false, false, false, false];
                             _currentIndex += 1;
